266281a031c58e28b5d89a167c241738fe79a916
Kyle <TheRealBanana@users.noreply.github.com>
2020-06-06 18:50:57 -0700

    Small changes
    Format of results changed slightly. Also updated useragent to be more up to date.

--- src/dialogs/logic/lyricsProviders/azlyrics.py
+++ src/dialogs/logic/lyricsProviders/azlyrics.py
@@ -4,7 +4,7 @@ import re
 from difflib import SequenceMatcher as sMatcher
 
 LYRICS_PROVIDER_NAME="AZlyrics"
-LYRICS_PROVIDER_VERSION="1.0"
+LYRICS_PROVIDER_VERSION="1.1"
 #Lyrics provider priority allows bLyrics to order lyrics providers properly. Lower numbered providers will be used
 #before higher numbered providers. Put the really slow ones at the end if you want cache generation to be quick.
 #Otherwise make the most reliable (in terms of lyrical content) the first priority. Providers with the same priority
@@ -24,8 +24,8 @@ class LyricsProvider(object):
 
         #Now lets execute the search and get the html returned so we can work on it
         downloaderHeaders = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.5",
             "Accept-Encoding": "deflate",
             "DNT": "1",
@@ -40,13 +40,12 @@ class LyricsProvider(object):
         except:
             return None
 
-        search_results = re.search("^.*Song results.*?<td class=\"text-left visitedlyr\">(.*?)(:?</table>).*</html>$", search_html.strip(), re.S|re.MULTILINE)
+        search_results = re.search("^.*Song results.*?<td class=\"text-left visitedlyr\".*?>(.*?)(?:</table>).*</html>$", search_html.strip(), re.S|re.MULTILINE)
         if search_results is not None:
-            split_results = search_results.group(1).split("<td class=\"text-left visitedlyr\">")
+            split_results = re.split("<td class=\"text-left visitedlyr\".*?>", search_results.group(1))
             for idx, result in enumerate(split_results):
-                result_url, result_songartist = re.search("%s\..*?<a href=\"(.*?)\" target=\"_blank\">((?:.*?)by(?:.*))" % str(idx+1), result).groups()
-                result_song = re.search("<b>(.*?)</b></a>", result_songartist).group(1)
-                result_artist = re.search("</a>\s+by\s+<b>(.*?)</b><br>", result_songartist).group(1)
+                result_url, result_songartist = re.search("%s\..*?<a href=\"(.*?)\">((?:.*?)(?:-|by)(?:.*))" % str(idx+1), result).groups()
+                result_song, result_artist = re.search("<b>\"(.*?)\"</b>\s+</a>\s+-\s+<b>(.*?)</b><br>", result_songartist).groups()
 
                 #Check if this result is the right one
                 if sMatcher(None, song.lower(), result_song.lower()).ratio() > self._MASTER_RATIO and sMatcher(None, artist.lower(), result_artist.lower()).ratio() > self._MASTER_RATIO:
